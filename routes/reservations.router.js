813b7967f0b3bff0454ad8b6a073e37edc269369
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-05-26 13:00:01 -0500

    Prueba metodo POST

--- routes/reservations.router.js
+++ routes/reservations.router.js
@@ -2,9 +2,12 @@ const express = require("express")
 
 const router = express.Router()
 
+let reservationsArray = []
+
 router.get("/", (req, res) => {
     res.status(200).json({
-        message: "Products"
+        message: "Products",
+        body: reservationsArray
     })
 })
 
@@ -12,8 +15,20 @@ router.get("/:id", (req, res) => {
     const { id } = req.params
     res.status(200).json({
         message: "Products",
-        product: id
+        product: id,
     })
+})
+
+router.post("/", (req, res) => {
+    let body = req.body
+    body = Object.values(body)
+    // const data = JSON.parse(body)
+    reservationsArray = [...body]
+    res.status(201).json(
+        {
+            body: reservationsArray
+        }
+    )
 })
 
 module.exports = router
